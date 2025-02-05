79a3c03719e93234dbc8f9f0d72675432b368aff
sarthak-tyagi123 <sarthaktyagi225@gmail.com>
2025-02-05 09:56:50 +0530

    rename

--- ce/file1.js
+++ ce/file1.js
@@ -1,9 +1,53 @@
 const fs=require("fs");
-// const data=fs.readFileSync("./data.txt","utf-8");
-// // console.log(data.toString()) ----- convert data into string
-// console.log(data)
+const data=fs.readFileSync("./data.txt","utf-8");
+// console.log(data.toString()) ----- convert data into string
+console.log(data)
+
+if(data.match("H"))
+{
+    console.log("H is present")
+    const changeddata=data.replace("H","ABES")
+    fs.writeFileSync("./data.txt", changeddata)
+}
 
 
-fs.writeFileSync("./data1.txt","created "); 
-fs.appendFileSync("./data1.txt","appended data")
-fs.renameSync("./data1.txt","./data2.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fs.writeFileSync("./data1.txt","created "); 
+// fs.appendFileSync("./data1.txt","appended data")
+// fs.renameSync("./data1.txt","./data2.txt")
+// fs.unlinkSync("./data2.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  git add .
+//  git commit -m "rename"
+//  git push origin main
