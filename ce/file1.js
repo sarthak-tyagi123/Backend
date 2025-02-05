85bb5ab0fd401c7b4a177e0fc4addbf74e7c5d76
sarthak-tyagi123 <sarthaktyagi225@gmail.com>
2025-02-05 09:46:17 +0530

    rename

--- ce/file1.js
+++ ce/file1.js
@@ -1,5 +1,9 @@
 const fs=require("fs");
-const data=fs.readFileSync("./data.txt","utf-8");
-// console.log(data.toString()) ----- convert data into string
-console.log(data)
+// const data=fs.readFileSync("./data.txt","utf-8");
+// // console.log(data.toString()) ----- convert data into string
+// console.log(data)
 
+
+fs.writeFileSync("./data1.txt","created "); 
+fs.appendFileSync("./data1.txt","appended data")
+fs.renameSync("./data1.txt","./data2.txt")
